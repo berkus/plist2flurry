ca4602df3e54241f00b3ccfbeafff973272a5759
Berkus <berkus@gmail.com>
2014-01-06 02:36:44 +0200

    Finish tool to convert plist to flurry serialized config.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
-#include <boost/program_options/>
-
+#include <map>
+#include <fstream>
 #include "flurry.h"
 #include "Plist.hpp"
 
+using namespace std;
+
+// run with args <infile.plist> <outfile.flurry>
+int main(int argc, char* argv[])
+{
+    map<string, boost::any> data;
+    assert(argc > 2);
+    ifstream is(argv[1], ios::in|ios::binary);
+    Plist::readPlist(is, data);
+    ofstream os(argv[2], ios::out|ios::binary|ios::trunc);
+    flurry::oarchive write(os);
+    write << data;
+}
